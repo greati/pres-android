cda5914daa14f553c6d60f02bb1be139498d41e9
Vitor Greati <greati@ufrn.edu.br>
2018-11-12 18:29:57 -0300

    Implement register screen

--- app/src/main/java/com/example/vitorgreati/presapp/LoginActivity.java
+++ app/src/main/java/com/example/vitorgreati/presapp/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.vitorgreati.presapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    public static final int REQ_CODE_REGISTER = 0;
+
+    private Button btRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btRegister = findViewById(R.id.btRegister);
+        btRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(v.getContext(), RegisterActivity.class);
+                startActivityForResult(i, REQ_CODE_REGISTER);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/vitorgreati/presapp/RegisterActivity.java
@@ -0,0 +1,29 @@
+package com.example.vitorgreati.presapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        getSupportActionBar().setTitle(R.string.register_title);
+
+        btnCancel = findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                setResult(RESULT_CANCELED);
+            }
+        });
+
+    }
+}
