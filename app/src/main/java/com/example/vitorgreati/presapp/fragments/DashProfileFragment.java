2c73fa765b7c262ac4b58e917574a2034c681df1
Vitor Greati <greati@ufrn.edu.br>
2018-11-20 16:47:34 -0300

    Set tab icon colors

--- app/src/main/java/com/example/vitorgreati/presapp/fragments/DashProfileFragment.java
+++ app/src/main/java/com/example/vitorgreati/presapp/fragments/DashProfileFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.vitorgreati.presapp.R;
 import com.example.vitorgreati.presapp.model.User;
@@ -33,6 +34,13 @@ public class DashProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.dash_profile_fragment, container, false);
+
+        TextView tvNome = v.findViewById(R.id.tvName);
+        TextView tvEmail = v.findViewById(R.id.tvEmail);
+
+        tvNome.setText(this.user.getName());
+        tvEmail.setText(this.user.getEmail());
+
         return v;
     }
 }
