87ff4cee1b8eef39c37a26f7c366cff0ec93cb43
Vitor Greati <greati@ufrn.edu.br>
2018-12-06 18:29:04 -0300

    Implement notification

--- app/src/main/java/com/example/vitorgreati/presapp/services/PresFirebaseMessagingService.java
+++ app/src/main/java/com/example/vitorgreati/presapp/services/PresFirebaseMessagingService.java
@@ -1,13 +1,18 @@
 package com.example.vitorgreati.presapp.services;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
 import com.example.vitorgreati.presapp.config.AppUtils;
+import com.example.vitorgreati.presapp.config.NotificationUtils;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -35,13 +40,20 @@ public class PresFirebaseMessagingService extends FirebaseMessagingService {
                     Intent i = new Intent(OPEN_QUESTION_ACTION);
                     i.putExtra("question_id",remoteMessage.getData().get("question_id"));
                     LocalBroadcastManager.getInstance(this).sendBroadcast(i);
+
+                    NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NotificationUtils.getChannelId(this));
+                    builder.setContentTitle("New open question!");
+                    builder.setContentText("Check your session window and answer the question.");
+                    builder.setSmallIcon(android.R.drawable.sym_action_chat);
+                    builder.setAutoCancel(true);
+                    Notification notification = builder.build();
+                    NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                    nm.notify(50, notification);
+
                     break;
             }
 
         }
-
-
-        Log.i("pres","Mensagem recebida!");
 
     }
 
